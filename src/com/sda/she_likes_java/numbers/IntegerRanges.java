9ab521048f113ea765acd5630f73d006af9954aa
Alma <ardaraviciute@yahoo.com>
2023-03-08 21:05:48 +0200

    float numbers ranges

--- src/com/sda/she_likes_java/numbers/IntegerRanges.java
+++ src/com/sda/she_likes_java/numbers/IntegerRanges.java
@@ -23,5 +23,8 @@ public class IntegerRanges {
         System.out.println("now let's check byte type");
         System.out.println("minimal byte number is: " + Byte.MIN_VALUE );
         System.out.println("maximum byte number is: " + Byte.MAX_VALUE);
-    }
+
+        System.out.println();
+
+            }
 }
