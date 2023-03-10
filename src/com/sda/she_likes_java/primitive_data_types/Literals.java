cd320f813f93cddf3a5c0c41ffb9331206ef5ff9
Alma <ardaraviciute@yahoo.com>
2023-03-10 21:19:25 +0200

    literals explained

---
+++ src/com/sda/she_likes_java/primitive_data_types/Literals.java
@@ -0,0 +1,21 @@
+package com.sda.she_likes_java.primitive_data_types;
+
+public class Literals {
+    public static void main(String[] args) {
+        // literal is a direct value
+        // string literal is e.g. "name"
+        String name = "Alma";
+
+        // integer literal
+        int age = 40;// 40 is of type int
+
+        // long literal
+        long anotherAge = 30L;
+
+        // double literal
+        double doubleNumber = 3.1;
+
+        // float literal
+        float floatNumber = 3.1F;
+    }
+}
