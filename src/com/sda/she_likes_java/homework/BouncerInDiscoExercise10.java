7664143af052ae315aa2f69cb80c6a3b093faeed
Alma <ardaraviciute@yahoo.com>
2023-03-13 01:53:54 +0200

    Exercise10 bouncer in disco Young and Old

---
+++ src/com/sda/she_likes_java/homework/BouncerInDiscoExercise10.java
@@ -0,0 +1,47 @@
+package com.sda.she_likes_java.homework;
+
+// A story about bouncers in a disco
+//Prepare a new class BouncersInDiscoStory and in it
+//- get the user’s age, ability to sing well and height from the console
+//- there are two bouncers in the disco: young and old
+//- Young checks if the person is adult, tall (or short :)) and can sing well
+//- Old has some kind of sight problem - so if any of the conditions are met, the person can enter the disco
+//- people try to cheat, so if someone is rejected by the young bouncer they try again with the old one:)
+
+import java.util.Scanner;
+
+public class BouncerInDiscoExercise10 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am young bouncer");
+        System.out.println("I am checking Your age, height and ability to sing");
+        System.out.println("First let me know Your age now");
+
+        Scanner inputReader = new Scanner(System.in);
+
+        String declineMessage = "Sorry, go home:)";
+        int age = inputReader.nextInt();
+        System.out.println("Now let me know Your height: ");
+        double height = inputReader.nextDouble();
+        System.out.println("Please sing to me now: ");
+        boolean canSing = inputReader.hasNextBoolean();
+
+        boolean isAllowedToPartyYoung = age >= 18 && height >= 2.0 && canSing;
+        boolean isNotAllowedToPartyYoung = age < 18 || height < 2.0 || !canSing;
+        boolean isAllowedToPartyOld = age >= 18 || height >= 2.0 || canSing;
+
+        if (isAllowedToPartyYoung) {
+            System.out.println("Have a great time at party!!!");
+            if (isNotAllowedToPartyYoung) {
+                System.out.println("Hello , I am old bouncer");
+                System.out.println("I am checking Your age, height and ability to sing");
+                System.out.println("Let me know Your age");
+                System.out.println("Now let me know Your height: ");
+                System.out.println("Please sing to me now: ");
+            } else {
+                System.out.println(isAllowedToPartyOld);
+            }
+        } else {
+            System.out.println(declineMessage);
+        }
+    }
+}
