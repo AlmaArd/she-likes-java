aade4024f9544474a678a41127fa285795712799
Alma <ardaraviciute@yahoo.com>
2023-03-20 21:48:27 +0200

    School grades - with array and for loop

---
+++ src/com/sda/she_likes_java/arrays/EnglishSchoolGrades.java
@@ -0,0 +1,18 @@
+package com.sda.she_likes_java.arrays;
+
+import java.util.Random;
+
+public class EnglishSchoolGrades {
+    public static void main(String[] args) {
+        int[] myGradesOnEnglishClass = new int[10000];
+
+        Random gradeGenerator = new Random(7);
+        // initializing grades in some random scoresc - created virtual grades
+        for (int index = 0; index < myGradesOnEnglishClass.length; index++) {
+            myGradesOnEnglishClass[index] = 1+ gradeGenerator.nextInt(6); //in PL max grade is 6; 1+ (6) - kad nebūtų 0
+        }
+        for (int index = 0; index < myGradesOnEnglishClass.length; index++) {
+            System.out.println("my grade on index nr: " + index + " is: " + myGradesOnEnglishClass[index]);
+        }
+    }
+}
