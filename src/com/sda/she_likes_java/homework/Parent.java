436c2b94141762436af9c5eb7ec236ee6ec46e6a
Alma <ardaraviciute@yahoo.com>
2023-03-24 23:17:01 +0200

    Start of Parent class

---
+++ src/com/sda/she_likes_java/homework/Parent.java
@@ -0,0 +1,35 @@
+package com.sda.she_likes_java.homework;
+/*Create a Parent class containing the following variables:
+- first name
+- surname
+- address - (use the class Address)
+- firstChild (use class Person)
+- secondChild (use class Person)
+ */
+public class Parent {
+String name;
+String surname;
+    public class Address {
+
+        String country;
+        String city;
+        String street;
+        int postalCode;
+        String postalCodeOther;
+    }
+    public class Person {
+        String name;
+        String surname;
+        int age;
+        String address;
+
+    }
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+}
