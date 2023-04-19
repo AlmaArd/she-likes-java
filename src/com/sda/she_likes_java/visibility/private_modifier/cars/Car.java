11c8bcad6d11ec4055b7ae536991ed0bfaafc58f
Alma <ardaraviciute@yahoo.com>
2023-04-19 20:31:47 +0300

    privates modifier

---
+++ src/com/sda/she_likes_java/visibility/private_modifier/cars/Car.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.visibility.private_modifier.cars;
+
+// it's not possisble to mark class as private
+public class Car {
+    private String brand;
+    public String model;
+
+
+    String colour;
+}
---
+++ src/com/sda/she_likes_java/visibility/private_modifier/main/Main.java
@@ -0,0 +1,17 @@
+package com.sda.she_likes_java.visibility.private_modifier.main;
+
+import com.sda.she_likes_java.visibility.private_modifier.cars.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car funnyCar = new Car();
+        // brand is private so not visible outside the class
+       // funnyCar.brand
+        funnyCar.model = "colorra";
+
+        // cannot see colour from outside its class package
+        // funnyCar.colour = "pink";
+
+    }
+
+}
