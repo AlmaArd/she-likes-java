158262d61f1fa66ccc3ef2164c9f903cbca5dbc9
Alma <ardaraviciute@yahoo.com>
2023-04-24 21:22:30 +0300

    Safe casting with instance of operator

--- src/com/sda/she_likes_java/virtual_methods/HumanLife.java
+++ src/com/sda/she_likes_java/virtual_methods/HumanLife.java
@@ -1,7 +1,34 @@
 package com.sda.she_likes_java.virtual_methods;
 
 public class HumanLife {
+
     public void tellMeWhoYouAre(Human human) {
-        human.whoYouAre();
+        if (human != null) {
+            human.whoYouAre();
+        }
+    }
+
+    public void tryToCastHumanToMan(Human maybeWomanOrMan) {
+
+        // instanceof operator - tests if argument is of given type
+        // tests if maybeWomanOrMan is of type Man
+        // instanceof gives us boolean result
+        if (maybeWomanOrMan instanceof Man) {
+            System.out.println("It's a momster:)");
+            // casting / down casting
+            // works for the references
+            (
+                    (Man) maybeWomanOrMan
+            ).learnToCleanHouse();
+
+            ((Man) maybeWomanOrMan).learnToCleanHouse();
+        }
+
+        if (maybeWomanOrMan instanceof Woman) {
+            System.out.println("It's a woman");
+            ((Woman) maybeWomanOrMan).learnJava();
+        }
     }
 }
+
+
--- src/com/sda/she_likes_java/virtual_methods/Man.java
+++ src/com/sda/she_likes_java/virtual_methods/Man.java
@@ -1,13 +1,13 @@
 package com.sda.she_likes_java.virtual_methods;
 
 public class Man extends Human {
+
     @Override
     public void whoYouAre() {
-        super.whoYouAre();
         System.out.println("I'm man");
+    }
 
-        public void learnToCleanHouse() {
-            System.out.println("I like it");
-        }
+    public void learnToCleanHouse() {
+        System.out.println("I like it");
     }
 }
