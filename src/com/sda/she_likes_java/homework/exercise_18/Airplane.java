f70eecf088dd04f63d22e64ffa5fde00e5f2dd60
Alma <ardaraviciute@yahoo.com>
2023-04-24 18:20:09 +0300

    Good exapmple of Excersise 18 - Airplane

--- src/com/sda/she_likes_java/homework/exercise_18/Airplane.java
+++ src/com/sda/she_likes_java/homework/exercise_18/Airplane.java
@@ -8,7 +8,7 @@ Create an Airplane class that provides the following operations:
 - 1)takeOff
 - 2)land
 - 3)flyTo(int xCoordinates, int yCoordinates)
-- 4)retrieve the current state of the aeroplane (flies/waits in hangar)
+- 4)retrieve the current state of the airplane (flies/waits in hangar)
 When implementing, remember that:
 - the aircraft must be in the air to land
 - you cannot take off if you are already in the air
@@ -16,38 +16,49 @@ When implementing, remember that:
 - check the use of your class in the main method
      */
 
-private int xCoordinates;
-private int yCoordinates;
-private int zCoordinates;
+    private boolean isFlying;
+    private int xCoordinatesOfDestiny;
+    private int yCoordinatesOfDestiny;
 
+    public boolean isFlying() {
+        return isFlying;
+    }
 
-public void takeOff() {
-    yCoordinates++;
-    System.out.println("Plane, fly up");
+    public int getxCoordinatesOfDestiny() {
+        return xCoordinatesOfDestiny;
+    }
+
+    public int getyCoordinatesOfDestiny() {
+        return yCoordinatesOfDestiny;
+    }
+
+    public void takeOff() {
+        if (!isFlying) {
+            isFlying = true;
+            System.out.println("take off in progress");
+        } else {
+            System.out.println("I've already started");
+        }
+    }
+
+    public void land() {
+        if (isFlying) {
+            isFlying = false;
+            System.out.println("landing in progress");
+        } else {
+            System.out.println("I've already landed");
+        }
+    }
+
+    public void flyTo(int xCoordinatesOfDestiny, int yCoordinatesOfDestiny) {
+        if (!isFlying) {
+            System.out.println("I need to take off first");
+        } else {
+            System.out.println("Ok, let's go to: [%d, %d]".formatted(xCoordinatesOfDestiny, yCoordinatesOfDestiny));
+            this.xCoordinatesOfDestiny = xCoordinatesOfDestiny;
+            this.yCoordinatesOfDestiny = yCoordinatesOfDestiny;
+        }
+    }
 }
 
-public void flyTo(){
-    xCoordinates++;
-    zCoordinates++;
-    System.out.println("Plane, fly to");
-
-}
-    public void land () {
-        yCoordinates--;
-        System.out.println("Plane, get down");
-    }
-
-    public Airplane(int xCoordinates, int yCoordinates, int zCoordinates) {
-        this.xCoordinates = xCoordinates;
-        this.yCoordinates = yCoordinates;
-        this.zCoordinates = zCoordinates;
-    }
-
-    public String airplaneFlies() {
-        return "";
-    }
-    public String airplaneWaitsInHangar() {
-        return "";
-    }
-}
 
