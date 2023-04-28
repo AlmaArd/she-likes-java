06d902598934a665c7c2df6a67081e588da65e1d
Alma <ardaraviciute@yahoo.com>
2023-04-28 19:51:11 +0300

    exercise 21 - start

---
+++ src/com/sda/she_likes_java/homework/exercise_21/Main.java
@@ -0,0 +1,23 @@
+package com.sda.she_likes_java.homework.exercise_21;
+
+public class Main {
+    public static void main(String[] args) {
+        Ship justShip = new Ship();
+        //ship reference = new Ship - object
+        justShip.move();
+        Vehicle ship = new Ship();
+        justShip.move();
+        ship.move();
+        // ship. swim();
+        ((Floating) ship).swim(); //Casting
+
+        Vehicle airplane = new Airplane();
+        airplane.move();
+        ((Flying) airplane).fly();
+
+        Vehicle submarine = new Submarine();
+        submarine.move();
+        ((Diving) submarine).dive();
+
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/exercise_21/Submarine.java
@@ -0,0 +1,8 @@
+package com.sda.she_likes_java.homework.exercise_21;
+
+public class Submarine extends Vehicle implements Floating {
+    @Override
+    public void swim() {
+
+    }
+}
