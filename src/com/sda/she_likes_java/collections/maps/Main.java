471b2840ea13c5b7de01da0a71a1746b9e9c44ab
Alma <ardaraviciute@yahoo.com>
2023-05-14 22:10:07 +0300

    map example:
    -contain values under the keys(value is represented by key)
    - any type can be type ot the key
    - any type can be type ot the value

--- src/com/sda/she_likes_java/collections/maps/Main.java
+++ src/com/sda/she_likes_java/collections/maps/Main.java
@@ -1,4 +1,27 @@
 package com.sda.she_likes_java.collections.maps;
 
+import com.sda.she_likes_java.collections.Person;
+import com.sda.she_likes_java.collections.sets.Trousers;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Maps are like dictionaries");
+
+        Map<String, Trousers> myJeans = new HashMap<>();
+        // key: "my favourite jeans"
+        // value: new Trousers("jeans", "big star")
+        myJeans.put("my favourite jeans", new Trousers("jeans", "big star"));
+        myJeans.put("my old one", new Trousers("unknown material", "primark"));
+        myJeans.put("my old one", new Trousers("unknown material", "primark II"));
+
+        Map<Person, Trousers> personsTrousers = new HashMap<>(); // HashMap - susmulkintas, sumaišytas žemėlapis
+        personsTrousers.put(new Person("mariusz", "p."), new Trousers("polish material", "primark"));
+        personsTrousers.put(new Person("karolina", "p."), new Trousers("polish material", "primark"));
+
+
+
+    }
 }
