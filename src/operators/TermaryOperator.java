0fa088acfc714e3a47b66c7da86068cef22bd97b
Alma <ardaraviciute@yahoo.com>
2023-05-17 20:09:16 +0300

    ternary operator

---
+++ src/operators/TermaryOperator.java
@@ -0,0 +1,37 @@
+package operators;
+
+// Tree wayoperator
+public class TermaryOperator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String getMyName(boolean fullName) {
+        // if condition is true return first part otherwise return second
+        String result = fullName ? "Alma Aradraviciute" : "Alma";
+        // return result;
+
+
+        String result2 = null;
+        if (fullName) {
+            result2 = "Long value";
+        } else {
+            result2 = "short";
+        }
+        // the same with ternary operator
+
+
+        return result;
+      //  if (fullNAme) {
+      //      return "alma Ardaraviciute";
+      //  } else {
+      //      return "Alma";
+        }
+
+    }
+
+
+
+
+
