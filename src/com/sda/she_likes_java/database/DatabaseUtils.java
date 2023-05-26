746c9baaa1936d7cb0b24e9e3402f665000304ed
Alma <ardaraviciute@yahoo.com>
2023-05-26 21:33:37 +0300

    Application for getting data from Test - almost working

--- src/com/sda/she_likes_java/database/DatabaseUtils.java
+++ src/com/sda/she_likes_java/database/DatabaseUtils.java
@@ -6,11 +6,34 @@ import java.sql.SQLException;
 
 public class DatabaseUtils {
 
+    /**
+     * Script for creating table
+     * CREATE TABLE TEST
+     * (ID INT PRIMARY KEY, NAME VARCHAR(255));
+     *
+     * Script for inserting one record(row) into table
+     * INSERT INTO
+     *  TEST(ID, NAME)
+     *  VALUES(1, 'Alma');
+     *
+     *  Script for reading data
+     *  SELECT ID, NAME
+     *  FROM TEST;
+     *
+     *  Script for updating data
+     * UPDATE TEST -- name of table to update
+     * SET NAME = 'Inese' -- column value to change
+     * WHERE ID=2 -- selected row
+     * Script for deleting data
+     * DELETE
+     * FROM TEST
+     * WHERE ID=3;
+    */
     // handle internally
     public static Connection getConnection () {
         // trows SQLExeption
         try {
-            return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/databases/jdbc", "sa", "");
+            return DriverManager.getConnection("jdbc:h2:~\\Data\\she-goes-tech", "sa", "sa");
         } catch (SQLException e) {
             return null;
         }
@@ -21,8 +44,8 @@ public class DatabaseUtils {
 
     // declare
     public static Connection getConnectionButSometimesThrowException() throws SQLException {
-        return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/databases/jdbc",
+        return DriverManager.getConnection("jdbc:h2:~\\Data\\she-goes-tech",
                 "sa",
-                "");
+                "sa");
     }
 }
--- src/com/sda/she_likes_java/homework/exercise_29/Main.java
+++ src/com/sda/she_likes_java/homework/exercise_29/Main.java
@@ -1,11 +1,9 @@
 package com.sda.she_likes_java.homework.exercise_29;
 
+//Create a class to check the postal code format with one method: void checkPostalCode(String postalCode).
+//The method returns nothing if the code format is correct.
+//Otherwise it throws a PostalCodeException exception.
+
 public class Main {
-//    public static void main(String[] args) {
-//
-//    }
-//    char mLetter = 'm';
-//        System.out.println("m letter: " + mLetter);
-//    int mLetterCode = mLetter;
-//        System.out.println("m letter code: " + mLetterCode);
+
 }
