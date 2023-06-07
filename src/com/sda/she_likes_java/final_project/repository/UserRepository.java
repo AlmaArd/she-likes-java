6ace3dca50885390a04f91de252f72ae6a4d31d1
Alma <ardaraviciute@yahoo.com>
2023-06-07 19:57:59 +0300

    Initial version of UserRepository

---
+++ src/com/sda/she_likes_java/final_project/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package com.sda.she_likes_java.final_project.repository;
+
+import com.sda.she_likes_java.final_project.User;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class UserRepository {
+
+    public void registerNewUser() {
+
+    }
+
+    public void deleteUser() {
+
+    }
+
+    public List<User> listAllUsers(){
+        return List.of();
+    }
+    public void updateUser(User user) {
+
+    }
+
+    public List<User> getUsersBasedOnCriteria() {
+        return List.of();
+    }
+    public List<User> getOrderedUsers(Comparator<User> comparator){
+        return List.of();
+    }
+}
