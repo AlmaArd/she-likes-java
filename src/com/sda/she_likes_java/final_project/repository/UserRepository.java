aa4ca31704e24398c2059df0ee832fff128dd249
Alma <ardaraviciute@yahoo.com>
2023-06-07 20:52:33 +0300

    register NewUser method Implemented

--- src/com/sda/she_likes_java/final_project/repository/UserRepository.java
+++ src/com/sda/she_likes_java/final_project/repository/UserRepository.java
@@ -1,31 +1,78 @@
 package com.sda.she_likes_java.final_project.repository;
 
 import com.sda.she_likes_java.final_project.User;
+import com.sda.she_likes_java.final_project.exception.InexperiencedUserException;
 
+import java.time.LocalDate;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserRepository {
 
-    public void registerNewUser() {
+    private static long userCounter;
+    private Map<Long, User> users;
 
+    public UserRepository() {
+        this.users = new HashMap<>();
+    }
+
+    public void registerNewUser(String name,
+                                String surname,
+                                LocalDate dateOfBirth,
+                                String login,
+                                String password,
+                                LocalDate drivingLicenseIssueDate) throws InexperiencedUserException {
+        checkDrivingLicense(drivingLicenseIssueDate);
+
+        Long id = nextId();
+
+        registerUser(name, surname, dateOfBirth, login, password, drivingLicenseIssueDate, id);
+    }
+
+    private void registerUser(String name, String surname, LocalDate dateOfBirth, String login, String password, LocalDate drivingLicenseIssueDate, Long id) {
+        User newUser = new User(id, name, surname, dateOfBirth, login, password, drivingLicenseIssueDate);
+        users.put(id, newUser);
+    }
+
+    private void checkDrivingLicense(LocalDate drivingLicenceIssueDate) throws InexperiencedUserException  {
+        // at least 3 years:)
+        final int requiredNumberOfYears = 3;
+        LocalDate currentDate = LocalDate.now();
+//        drivingLicenceIssueDate + 3 year is before now
+        LocalDate boundaryDate = drivingLicenceIssueDate.plusYears(requiredNumberOfYears);
+
+        if (boundaryDate.isAfter(currentDate)) {
+            throw new InexperiencedUserException("Better buy a bicycle!");
+        }
+    }
+
+    private static long nextId() {
+        return ++userCounter;
     }
 
     public void deleteUser() {
+        // TODO
 
     }
-
-    public List<User> listAllUsers(){
+    // TODO
+    public List<User> listAllUsers() {
         return List.of();
     }
+
     public void updateUser(User user) {
+    // TODO
 
     }
 
+    // TODO
     public List<User> getUsersBasedOnCriteria() {
         return List.of();
     }
-    public List<User> getOrderedUsers(Comparator<User> comparator){
+
+    // TODO
+    public List<User> getOrderedUsers(Comparator<User> comparator) {
         return List.of();
     }
 }
